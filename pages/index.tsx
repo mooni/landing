4a9170372f4418389bc9a7887b7d983146c487bd
Pacien Boisson <pakokrew@gmail.com>
2021-03-01 12:37:12 +0400

    Cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout'
+import Link from 'next/link'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section>
+        <h2>Blog</h2>
+      </section>
+    </Layout>
+  )
+}
