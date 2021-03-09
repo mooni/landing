5eeed1ab1ef9777dfc4d3785d1281dc648164a95
Pacien Boisson <pakokrew@gmail.com>
2021-03-09 11:33:20 +0400

    fix fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
 
 import chakraTheme from '../theme'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function App({ Component, pageProps }: AppProps) {
   return (
